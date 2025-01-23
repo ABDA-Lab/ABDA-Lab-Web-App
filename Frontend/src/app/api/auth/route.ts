260a71f6f9f3b181f8230a9164b788b37de53c4b
andy.ninety9 <duy.maianh26@gmail.com>
2025-01-23 12:38:56 +0700

    WIP: Login feature

--- Frontend/src/app/api/auth/route.ts
+++
@@ -1,6 +0,0 @@
-import { LoginBodyType, LoginResType } from "@/schemaValidations/auth.schema";
-
-
-export const login = async (body: LoginBodyType): Promise<LoginResType> => {
-	throw new Error('Not implemented');
-}
