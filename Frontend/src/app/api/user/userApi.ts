e07cf850cf616ff9a3675a07555ef57a7e45a3c9
andy.ninety9 <duy.maianh26@gmail.com>
2025-02-05 07:23:03 +0700

    UPDATE: Get profile UI

--- Frontend/src/app/api/user/userApi.ts
+++ Frontend/src/app/api/user/userApi.ts
@@ -1,16 +1,16 @@
 import http from "@/lib/http";
-import { GetMeResType } from "@/schemaValidations/user.schema";
+import { GetMeResType, UserSchemaType } from "@/schemaValidations/user.schema";
 
 export const getMe = async (): Promise<GetMeResType> => {
     const response = await http.get<{
         statusCode: number;
         message: string;
-        data: GetMeResType;
+        data: UserSchemaType;
     }>('api/user/get-me');
 
     if (response.statusCode !== 200) {
         throw new Error(response.message || 'Failed to fetch user profile');
     }
 
-    return response.data; 
+    return response; 
 };
--- Frontend/src/schemaValidations/user.schema.ts
+++ Frontend/src/schemaValidations/user.schema.ts
@@ -3,29 +3,36 @@ import z from 'zod';
 // ✅ Define Request Body Schema for GetMe
 
 export const GetMeRes = z.object({
-    value: z
-        .object({
-            userid: z.string(),
-            username: z.string(),
-            fullname: z.string().nullable(),
-            nickname: z.string().nullable(),
-            birthday: z.string().nullable(),
-            country: z.string().nullable(),
-            description: z.string().nullable(),
-            profilemd: z.string().nullable(),
-            avatar: z.string().nullable(),
-            status: z.string().nullable(),
-            createdat: z.string(),
-        })
-        .optional(),
-    isSuccess: z.boolean(),
-    isFailure: z.boolean(),
-    error: z
-        .object({
-            code: z.string().nullable(),
-            description: z.string().nullable(),
-        })
-        .nullable(),
+    statusCode: z.number(),
+    message: z.string(),
+    data: z.object({
+        userid: z.string(),
+        username: z.string(),
+        fullname: z.string().nullable(),
+        nickname: z.string().nullable(),
+        birthday: z.string().nullable(),
+        country: z.string().nullable(),
+        description: z.string().nullable(),
+        profilemd: z.string().nullable(),
+        avatar: z.string().nullable(),
+        status: z.string().nullable(),
+        createdat: z.string(),
+    })
 });
 
+export const UserSchema = z.object({
+    userid: z.string(),
+    username: z.string(),
+    fullname: z.string().nullable(),
+    nickname: z.string().nullable(),
+    birthday: z.string().nullable(),
+    country: z.string().nullable(),
+    description: z.string().nullable(),
+    profilemd: z.string().nullable(),
+    avatar: z.string().nullable(),
+    status: z.string().nullable(),
+    createdat: z.string(),
+});
+
+export type UserSchemaType = z.infer<typeof UserSchema>;
 export type GetMeResType = z.infer<typeof GetMeRes>;
