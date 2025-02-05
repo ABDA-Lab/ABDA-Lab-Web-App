ad78e98b766392c092186bddbeab1109ad686be4
andy.ninety9 <duy.maianh26@gmail.com>
2025-02-05 11:33:38 +0700

    UPDATE: edit gitignore

--- Frontend/.history/src/components/Navigation_20250118232743.tsx
+++
@@ -1,9 +0,0 @@
-export default function Navigation() {
-    return (
-        <div className="">
-            <MenuItem item="" active={active} setActive={setActive}>
-                    Home
-                </MenuItem>
-        </div>
-    );
-}
