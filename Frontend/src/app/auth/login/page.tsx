5653bc99a5d5d53eab3290e9b7a76350f9cd9e19
andy.ninety9 <duy.maianh26@gmail.com>
2025-02-05 10:07:55 +0700

    UPDATE: add validation for login page

--- Frontend/src/app/auth/login/page.tsx
+++ Frontend/src/app/auth/login/page.tsx
@@ -5,21 +5,25 @@ export const metadata: Metadata = {
     title: 'Login',
     description: 'Login page',
 };
+
 export default function LoginPage() {
     return (
-        <div className="grid min-h-svh lg:grid-cols-2">
-            <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="grid h-screen lg:grid-cols-2 overflow-hidden">
+            {/* Form Container */}
+            <div className="flex flex-col gap-4 p-4 md:p-8">
                 <div className="flex flex-1 items-center justify-center">
-                    <div className="w-full max-w-xs">
+                    <div className="w-full max-w-sm">
                         <LoginForm />
                     </div>
                 </div>
             </div>
-            <div className="relative hidden bg-muted lg:block">
+
+            {/* Background Image */}
+            <div className="relative hidden bg-muted lg:flex items-center justify-center">
                 <img
                     src="https://images.unsplash.com/photo-1736117024365-c36ccb9f843d?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                     alt="ImageLogin"
-                    className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                    className="h-full w-full object-cover object-center"
                 />
             </div>
         </div>
