7351c132f74f2c20474c1c67adf9479b198f739c
andy.ninety9 <duy.maianh26@gmail.com>
2025-02-05 10:07:55 +0700

    UPDATE: add validation for login page

--- Frontend/src/components/authentication/loginForm.tsx
+++ Frontend/src/components/authentication/loginForm.tsx
@@ -7,6 +7,9 @@ import { Label } from '@/components/ui/label';
 import { useState } from 'react';
 import { useLogin } from '@/hooks/authentication/useLogin';
 import Link from 'next/link';
+import { LoginBody, LoginBodyType } from '@/schemaValidations/auth.schema';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRef<'form'>) {
     const [username, setUsername] = useState('');
@@ -14,13 +17,21 @@ export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRe
     const [showPassword, setShowPassword] = useState(false);
     const { handleLogin, loading } = useLogin();
 
-    const onSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        handleLogin(username, password);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<LoginBodyType>({
+        resolver: zodResolver(LoginBody),
+    });
+
+    // Hàm xử lý submit form
+    const onSubmit = (data: LoginBodyType) => {
+        handleLogin(data.username, data.password);
     };
 
     return (
-        <form className={cn('flex flex-col gap-6', className)} onSubmit={onSubmit} {...props}>
+        <form className={cn('flex flex-col gap-6', className)} onSubmit={handleSubmit(onSubmit)} {...props}>
             <div className="flex flex-col items-center gap-2 text-center">
                 <h1 className="text-2xl font-bold">Login to your account</h1>
                 <p className="text-balance text-sm text-muted-foreground">
@@ -30,43 +41,59 @@ export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRe
             <div className="grid gap-6">
                 <div className="grid gap-2">
                     <Label htmlFor="email">Username</Label>
-                    <Input
-                        id="email"
-                        type="text"
-                        placeholder="m@example.com"
-                        required
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
+                    <Input id="username" type="text" placeholder="Enter your username" {...register('username')} />
+                    <div className="min-h-5">
+                        {errors.username && <p className="text-red-500 text-sm">{errors.username.message}</p>}
+                    </div>
                 </div>
                 <div className="grid gap-2">
                     <Label htmlFor="password">Password</Label>
                     <div className="relative">
                         <Input
                             id="password"
-                            type={showPassword ? "text" : "password"}
-                            required
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
+                            type="password"
+                            placeholder="Enter your password"
+                            {...register('password')}
                         />
+
                         <Button
                             type="button"
                             variant="ghost"
                             className="absolute right-0 top-0 h-full px-3"
-                            onClick={() => setShowPassword(!showPassword)}
-                        >
+                            onClick={() => setShowPassword(!showPassword)}>
                             {showPassword ? (
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="20"
+                                    height="20"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round">
                                     <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path>
                                     <line x1="1" y1="1" x2="23" y2="23"></line>
                                 </svg>
                             ) : (
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="20"
+                                    height="20"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round">
                                     <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
                                     <circle cx="12" cy="12" r="3"></circle>
                                 </svg>
                             )}
                         </Button>
+                    </div>
+                    <div className="min-h-10">
+                        {errors.password && <p className="text-red-500 text-sm">{errors.password.message}</p>}
                     </div>
                 </div>
                 <Button type="submit" className="w-full" disabled={loading}>
