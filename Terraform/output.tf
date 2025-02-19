4115da29d987cead14d7ee211670ccf107a4aeb2
Vkev <huynhkhang7452@gmail.com>
2025-02-19 19:31:56 +0700

    Add Ecs and Vpc, Subnet terraform for test

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -7,3 +7,18 @@ output "cloudfront_domain_name" {
   description = "The domain name of the CloudFront distribution"
   value       = module.cloudfront.cloudfront_domain_name
 }
+
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "ecs_cluster_id" {
+  description = "ID of the ECS cluster"
+  value       = module.ecs.ecs_cluster_id
+}
+
+output "ecs_asg_id" {
+  description = "ID of the Auto Scaling Group for ECS instances"
+  value       = module.ecs.asg_id
+}
