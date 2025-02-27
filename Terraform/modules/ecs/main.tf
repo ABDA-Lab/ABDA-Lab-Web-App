f5d17676a4e0a67ddc69ea88e0fa00aad024d6a3
Vkev <huynhkhang7452@gmail.com>
2025-02-27 22:57:15 +0700

    Add ssm

--- Terraform/modules/ecs/main.tf
+++ Terraform/modules/ecs/main.tf
@@ -88,3 +88,8 @@ resource "aws_autoscaling_group" "ecs_asg" {
     version = "$Latest"
   }
 }
+
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
