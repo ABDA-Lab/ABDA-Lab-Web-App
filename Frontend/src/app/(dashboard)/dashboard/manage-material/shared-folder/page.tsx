13630d299a1ba01f3b9ce504dfb0439368609c97
Mai Anh Duy <114195582+andyninety9@users.noreply.github.com>
2025-04-06 14:08:07 +0700

    ADD: learning page

--- Frontend/src/app/(dashboard)/dashboard/manage-material/shared-folder/page.tsx
+++ Frontend/src/app/(dashboard)/dashboard/manage-material/shared-folder/page.tsx
@@ -3,5 +3,9 @@
 import SharingFileTab from '@/components/dashboard/SharingFileTab';
 
 export default function page() {
-    return <SharingFileTab />;
+    return (
+        <div className="container mx-auto p-4 space-y-6">
+            <SharingFileTab />
+        </div>
+    );
 }
