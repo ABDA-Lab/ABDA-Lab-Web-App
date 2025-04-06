fb9dd311c39db7741a3ac3dc5397cd72f90c061a
Mai Anh Duy <114195582+andyninety9@users.noreply.github.com>
2025-04-06 23:44:45 +0700

    ADD: sidebar state

--- Frontend/src/app/(dashboard)/dashboard/layout.tsx
+++ Frontend/src/app/(dashboard)/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+'use client';
 import type { Metadata } from 'next';
 import { JetBrains_Mono } from 'next/font/google';
 import '@/styles/globals.css';
@@ -12,25 +13,25 @@ const jetBrainsMono = JetBrains_Mono({
     subsets: ['latin'],
 });
 
-export const metadata: Metadata = {
-    title: 'ABDA Lab',
-    description: 'Lab at FPT University',
-};
+// export const metadata: Metadata = {
+//     title: 'ABDA Lab',
+//     description: 'Lab at FPT University',
+// };
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
         // <div className="dashboard-layout">
-            <Provider>
-                <SidebarProvider>
-                    <AppSidebar />
-                    <SidebarInset className="w-full flex-1">
-                        <HeaderDashboard />
-                        <main className="flex-1 w-full">{children}</main>
-                        {/* {children} */}
-                    </SidebarInset>
-                </SidebarProvider>
-                <Toaster position="top-right" reverseOrder={false} />
-            </Provider>
+        <Provider>
+            <SidebarProvider>
+                <AppSidebar />
+                <SidebarInset>
+                    <HeaderDashboard />
+                    <main className="flex-1 w-full">{children}</main>
+                    {/* {children} */}
+                </SidebarInset>
+            </SidebarProvider>
+            <Toaster position="top-right" reverseOrder={false} />
+        </Provider>
         // </div>
     );
 }
